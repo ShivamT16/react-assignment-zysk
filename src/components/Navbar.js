f96f03c18fcc115723274c2679c688276bb33e0c
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-12-30 22:43:16 +0530

    Navbar defined

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+export const Navbar = () => {
+    return(
+      <nav className="flex justify-between px-[5%] border-b p-4">
+
+       <div className="flex justify-around w-1/2 items-center">
+        <p className="text-xl font-semibold"> Untitled UI </p>
+        <p> Home </p>
+        <p> Products ⋎ </p>
+        <p> Resources ⋎ </p>
+        <p> Pricing </p>
+       </div>
+       
+       <div>
+        <p>image</p>
+       </div>
+
+      </nav>
+)}
